a7a8a642cf060c7b724900203d0e7ec376447347
GhassanDabak <ghassan.h.dabak@gmail.comgit config --global user.name GhassanDabakgit config --global user.email ghassan.h.dabak@gmail.com>
2021-12-24 20:32:41 +0200

    added header

--- src/App.js
+++ src/App.js
@@ -1,12 +1,10 @@
 import "./App.css";
+import Header from "./components/Header/Header";
 
 function App() {
   return (
     <div className="App">
-      <h1>Hello Ahmad From Branch Ahmad</h1>
-      <h1>Hello Teammates</h1>
-      <p>Hello From Abdullah Branch</p>
-      <p>Hello Check</p>
+      <Header />
     </div>
   );
 }
