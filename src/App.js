6d647c32a4f71bd0fe9e83016f6ede9ebe03f48d
AbdallahMarei <abdullahmari@gmail.com>
2021-12-24 17:52:38 +0200

    finished footer

--- src/App.js
+++ src/App.js
@@ -1,12 +1,10 @@
 import "./App.css";
+import Footer from "./components/Footer/Footer";
 
 function App() {
   return (
     <div className="App">
-      <h1>Hello Ahmad From Branch Ahmad</h1>
-      <h1>Hello Teammates</h1>
-      <p>Hello From Abdullah Branch</p>
-      <p>Hello Check</p>
+      <Footer />
     </div>
   );
 }
---
+++ src/components/Footer/Footer.js
@@ -0,0 +1,36 @@
+import React from 'react'
+import "./Footer.css"
+
+function Footer() {
+    return (
+        <footer>
+            <div className='footer-container'>
+        <div className="logo-and-icons-container">
+          <img src="assets/i-FIX LOGO.png" alt='logo' />
+          <p className="footer-Paragraph">
+            Lorem Ipsum is simply dummy text of the
+            <br /> printing and typesetting industry.
+            <br />Lorem Ipsum has been the industry's
+            <br />standard dummy text ever since the 1500s.
+          </p>
+          <div className="footerIcons">
+                <a href="https://linkedin.com/"><i className="fab fa-linkedin-in"></i></a>
+                <a href="https://github.com/"><i class="fab fa-github"></i></a>
+                <a href="https://twitter.com/"><i className="fab fa-twitter"></i></a>
+          </div>
+        </div>
+        <div className="footer-contactUs">
+          <div className='footer-contact-info'>
+           <h1 className="contatUsTitle">Contact US</h1>
+            <p className="contatUsPara"><i class="fas fa-map-marker-alt"></i> Amman/Jordan</p>
+            <p className="contatUsPara"><i class="fas fa-phone-alt"></i> +962798861423</p>
+            <p className="contatUsPara"><i class="fas fa-envelope"></i> ifix@gmail.com</p>
+          </div>
+        </div>
+        </div>
+        <p className='copyright'>Copyright ©2021 All rights reserved</p>
+        </footer>
+    )
+}
+
+export default Footer
