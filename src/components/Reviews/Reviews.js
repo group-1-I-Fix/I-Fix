78a9354a2989790c30dbe9de0c88ba01c06b67da
DeekCS <31720001079@std.bau.edu.jo>
2021-12-25 12:33:08 +0200

    fix testimonial

--- src/components/Reviews/Reviews.js
+++ src/components/Reviews/Reviews.js
@@ -2,164 +2,84 @@ import React from "react";
 import OwlCarousel from "react-owl-carousel";
 import "owl.carousel/dist/assets/owl.carousel.css";
 import "owl.carousel/dist/assets/owl.theme.default.css";
-import { Link } from "react-router-dom";
+import {Link} from "react-router-dom";
 import Swal from "sweetalert2";
 import "./reviews.css";
+import {Carousel} from "react-bootstrap";
+
 
 const images = [
-  {
-    image1: "assets/1.jpg",
-    alt: "image1",
-  },
-  {
-    image2: "assets/2.jpg",
-    alt: "image2",
-  },
-  {
-    image3: "assets/3.jpg",
-    alt: "image3",
-  },
+    {
+        image1: "assets/1.jpg",
+        alt: "image1",
+    },
+    {
+        image2: "assets/2.jpg",
+        alt: "image2",
+    },
+    {
+        image3: "assets/3.jpg",
+        alt: "image3",
+    },
 ];
 
-const options = {
-  // items: 1,
-  navigation: true, // Show next and prev buttons
-  slideSpeed: 300,
-  paginationSpeed: 400,
-  singleItem: true,
-  navText: [
-    "<i class='fa fa-angle-left'></i>",
-    "<i class='fa fa-angle-right'></i>",
-  ],
-};
-
-function Reviews() {
-  //options for slider owl
-
-  return (
-    <>
-        <div className="Services pd-y">
-          <div className="section-header">
-            <h2 className="section-title">Testimonial</h2>
-            <span className="line"/>
-          </div>
-          <div id="carouselExampleControls" className="carousel slide" data-ride="carousel">
-            <div className="carousel-inner">
-              <div className="carousel-item active">
-                <div className="testimonial-container">
-                  <div className="testimonial-item ">
-                    <div className="testimonial-item-perhour">
-                      <img src="https://www.toolshero.com/wp-content/uploads/2020/12/mark-zuckerberg-toolshero.jpg"
-                           alt="image1"/>
-                    </div>
-                    <p className="testimonial-list">
-                      <span>Mark Zuckerberg</span>
-                      <span>CEO - Facebook</span>
-                      <span>The Landscaping Professionals were quick, courteous and very helpful </span>
-                    </p>
-                  </div>
-                </div>
-              </div>
-              <div className="carousel-item active">
-                <div className="testimonial-container">
-                  <div className="testimonial-item ">
-                    <div className="testimonial-item-perhour">
-                      <img src="https://www.toolshero.com/wp-content/uploads/2020/12/mark-zuckerberg-toolshero.jpg"
-                           alt="image1"/>
-                    </div>
-                    <p className="testimonial-list">
-                      <span>Mark Zuckerberg</span>
-                      <span>CEO - Facebook</span>
-                      <span>The Landscaping Professionals were quick, courteous and very helpful </span>
-                    </p>
-                  </div>
-                </div>
-              </div>
+const  Reviews = () => {
+    return (
+        <>
+            <div className="Services pd-y">
+                <Carousel variant="dark">
+                    <Carousel.Item>
+                        <div className="testimonial-container">
+                            <div className="testimonial-item ">
+                                <div className="testimonial-item-perhour">
+                                    <img
+                                        src="https://upload.wikimedia.org/wikipedia/commons/1/18/Mark_Zuckerberg_F8_2019_Keynote_%2832830578717%29_%28cropped%29.jpg"
+                                        alt="image1"/>
+                                </div>
+                                <p className="testimonial-list">
+                                    <span>Mark Zuckerberg</span>
+                                    <span>CEO - Facebook</span>
+                                    <span>The Landscaping Professionals were quick, courteous and very helpful </span>
+                                </p>
+                            </div>
+                        </div>
+                    </Carousel.Item>
+                    <Carousel.Item>
+                        <div className="testimonial-container">
+                            <div className="testimonial-item ">
+                                <div className="testimonial-item-perhour">
+                                    <img
+                                        src="https://assets.telegraphindia.com/telegraph/2020/Dec/1607925942_bill-gates.jpg"
+                                        alt="image1"/>
+                                </div>
+                                <p className="testimonial-list">
+                                    <span>Bill Gates</span>
+                                    <span>CEO - Microsoft</span>
+                                    <span>The Landscaping Professionals were quick, courteous and very helpful </span>
+                                </p>
+                            </div>
+                        </div>
+                    </Carousel.Item>
+                    <Carousel.Item>
+                        <div className="testimonial-container">
+                            <div className="testimonial-item ">
+                                <div className="testimonial-item-perhour">
+                                    <img
+                                        src="https://www.biography.com/.image/ar_1:1%2Cc_fill%2Ccs_srgb%2Cfl_progressive%2Cq_auto:good%2Cw_1200/MTc5OTk2ODUyMTMxNzM0ODcy/gettyimages-1229892983-square.jpg"
+                                        alt="image1"/>
+                                </div>
+                                <p className="testimonial-list">
+                                    <span>Elon Musk</span>
+                                    <span>CEO - Tesla</span>
+                                    <span>The Landscaping Professionals were quick, courteous and very helpful </span>
+                                </p>
+                            </div>
+                        </div>
+                    </Carousel.Item>
+                </Carousel>
             </div>
-            <a className="carousel-control-prev" href="#carouselExampleControls" role="button" data-slide="prev">
-              <span className="carousel-control-prev-icon" aria-hidden="false">&#10094;</span>
-              {/*<span className="sr-only">&#10094;</span>*/}
-            </a>
-            <a className="carousel-control-next" href="#carouselExampleControls" role="button" data-slide="next">
-              <span className="carousel-control-next-icon" aria-hidden="false">&#10095;</span>
-              {/*<span className="sr-only">&#10095;</span>*/}
-            </a>
-          </div>
-        </div>
-    </>
-  );
+        </>
+    );
 }
 
 export default Reviews;
-
-/*
-<div id="carouselExampleControls" class="carousel slide" data-ride="carousel">
-  <div class="carousel-inner">
-    <div class="carousel-item active">
-           <div className="testimonial-container">
-            <div className="testimonial-item tb-effect">
-              <span className="testimonial-item-text">
-                {" "}
-                Electricity maintenance
-              </span>
-              <div className="testimonial-item-perhour">
-                  <img  src="https://www.toolshero.com/wp-content/uploads/2020/12/mark-zuckerberg-toolshero.jpg" alt="image1" />
-              </div>
-              <ul className="testimonial-list ">
-                <li>General Maintenance</li>
-                <li>Home Extensions</li>
-                <li>24/24 Support</li>
-              </ul>
-              <button className="testimonial-item-purchase">Book Now</button>
-            </div>
-          </div>
-    </div>
-    <div class="carousel-item">
-           <div className="testimonial-container">
-            <div className="testimonial-item tb-effect">
-              <span className="testimonial-item-text">
-                {" "}
-                Electricity maintenance
-              </span>
-              <div className="testimonial-item-perhour">
-                  <img  src="https://www.toolshero.com/wp-content/uploads/2020/12/mark-zuckerberg-toolshero.jpg" alt="image1" />
-              </div>
-              <ul className="testimonial-list ">
-                <li>General Maintenance</li>
-                <li>Home Extensions</li>
-                <li>24/24 Support</li>
-              </ul>
-              <button className="testimonial-item-purchase">Book Now</button>
-            </div>
-          </div>
-    </div>
-    <div class="carousel-item">
-           <div className="testimonial-container">
-            <div className="testimonial-item tb-effect">
-              <span className="testimonial-item-text">
-                {" "}
-                Electricity maintenance
-              </span>
-              <div className="testimonial-item-perhour">
-                  <img  src="https://www.toolshero.com/wp-content/uploads/2020/12/mark-zuckerberg-toolshero.jpg" alt="image1" />
-              </div>
-              <ul className="testimonial-list ">
-                <li>General Maintenance</li>
-                <li>Home Extensions</li>
-                <li>24/24 Support</li>
-              </ul>
-              <button className="testimonial-item-purchase">Book Now</button>
-            </div>
-          </div>
-    </div>
-  </div>
-  <a class="carousel-control-prev" href="#carouselExampleControls" role="button" data-slide="prev">
-    <span class="carousel-control-prev-icon" aria-hidden="true"></span>
-    <span class="sr-only">Previous</span>
-  </a>
-  <a class="carousel-control-next" href="#carouselExampleControls" role="button" data-slide="next">
-    <span class="carousel-control-next-icon" aria-hidden="true"></span>
-    <span class="sr-only">Next</span>
-  </a>
-</div>
-*/
