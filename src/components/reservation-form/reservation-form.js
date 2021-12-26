271c5d47ace4d78479a19b47109bdefd567a7c05
DeekCS <31720001079@std.bau.edu.jo>
2021-12-26 15:26:46 +0200

    Update

--- src/components/reservation-form/reservation-form.js
+++ src/components/reservation-form/reservation-form.js
@@ -1,97 +1,90 @@
-import React, {useState, useEffect} from "react";
+import React, { useState, useEffect } from "react";
 import Userprofile from "../../pages/Home/UserProfile/Userprofile";
 import "./reservation-form.css";
-import Swal from 'sweetalert2/dist/sweetalert2.js';
-import 'sweetalert2/src/sweetalert2.scss';
+import Swal from "sweetalert2/dist/sweetalert2.js";
+import "sweetalert2/src/sweetalert2.scss";
 
-function ReservationForm({service}) {
-  const loggedUserNow = JSON.parse(localStorage.getItem("loggedUser"))
+function ReservationForm({ service }) {
   let [appointments, setAppointments] = useState(
     JSON.parse(localStorage.getItem(`${service.title} appointments`))
       ? JSON.parse(localStorage.getItem(`${service.title} appointments`))
       : []
   );
   const [reservation, setReservation] = useState({
-    mobile: "",
     date: "",
     startTime: "",
     finishTime: "",
   });
-  const [newSTime,setNewSTime] = useState("")
-  const [newFTime,setNewFTime] = useState("")
-  
+  const [newSTime, setNewSTime] = useState("");
+  const [newFTime, setNewFTime] = useState("");
 
   useEffect(() => {
     let newStartTime = reservation.startTime.split("");
     newStartTime.splice(2, 1);
     let newStartTimeString = newStartTime.join("");
-    setNewSTime(newStartTimeString)
+    setNewSTime(newStartTimeString);
 
     let newFinishTime = reservation.finishTime.split("");
     newFinishTime.splice(2, 1);
     let newFinishTimeString = newFinishTime.join("");
-    setNewFTime(newFinishTimeString)
-  },[reservation.finishTime,reservation.startTime])
-
+    setNewFTime(newFinishTimeString);
+  }, [reservation.finishTime, reservation.startTime]);
 
   const handleChange = (e) => {
     const { name, value } = e.target;
     setReservation({ ...reservation, [name]: value });
-
   };
   const handleSubmit = (e) => {
     e.preventDefault();
     let newStartTime = reservation.startTime.split("");
     newStartTime.splice(2, 1);
     let newStartTimeString = newStartTime.join("");
-    
 
     let newFinishTime = reservation.finishTime.split("");
     newFinishTime.splice(2, 1);
     let newFinishTimeString = newFinishTime.join("");
-  
 
     if (Number(newFinishTimeString) < Number(newStartTimeString)) {
-      alert("please change time");
-      // sweet alert end time is less than start time (please pick time)
+      Swal.fire({
+        title: "Oops...",
+        text: "Please pick a time that is after the start time",
+        icon: "error",
+        confirmButtonText: "OK",
+      });
       return;
     }
 
     let flag = false;
 
-    const newTotalPrice = ((Number(newFinishTimeString) - Number(newStartTimeString)) / 100) * service.price;
-    const newId = 1 + new Date();
+    const newTotalPrice =
+      ((Number(newFinishTimeString) - Number(newStartTimeString)) / 100) *
+      service.price;
 
     let newAppointment = {
-      mobileNumber: reservation.mobile,
-      id: newId,
       service: service.title,
       date: reservation.date,
       startTime: newStartTimeString,
       finishTime: newFinishTimeString,
-      totalPrice: newTotalPrice
+      totalPrice: newTotalPrice,
     };
-    const handleSubmit = (e) => {
-        e.preventDefault();
-        let newStartTime = reservation.startTime.split("");
-        newStartTime.splice(2, 1);
-        let newStartTimeString = newStartTime.join("");
 
-
-        let newFinishTime = reservation.finishTime.split("");
-        newFinishTime.splice(2, 1);
-        let newFinishTimeString = newFinishTime.join("");
-
-
-        if (Number(newFinishTimeString) < Number(newStartTimeString)) {
-            Swal.fire({
-                title: "Oops...",
-                text: "Please pick a time that is after the start time",
-                icon: "error",
-                confirmButtonText: "OK"
-            })
-            return;
+    let newArray = appointments;
+    newArray.forEach((item) => {
+      if (item.date === newAppointment.date) {
+        if (
+          Number(newAppointment.finishTime) > Number(item.startTime) &&
+          Number(newAppointment.startTime) < Number(item.finishTime)
+        ) {
+          Swal.fire({
+            title: "Oops...",
+            text: "Please pick a time that is not already booked",
+            icon: "error",
+            confirmButtonText: "OK",
+          });
+          flag = true;
         }
+      }
+    });
 
     if (!flag) {
       newArray.push(newAppointment);
@@ -99,17 +92,22 @@ function ReservationForm({service}) {
         `${service.title} appointments`,
         JSON.stringify(newArray)
       );
-      const user = JSON.parse(localStorage.getItem("loggedUser"))
-      user.appointments.push(newAppointment)
-      localStorage.setItem("loggedUser" , JSON.stringify(user))
+      const user = JSON.parse(localStorage.getItem("loggedUser"));
+      user.appointments.push(newAppointment);
+      localStorage.setItem("loggedUser", JSON.stringify(user));
       const allUsers = JSON.parse(localStorage.getItem("users"));
-      const filteredAllUsers = allUsers.filter(data => user.id !== data.id)
+      const filteredAllUsers = allUsers.filter((data) => user.id !== data.id);
       filteredAllUsers.push(user);
-      localStorage.setItem("users", JSON.stringify(filteredAllUsers))
+      localStorage.setItem("users", JSON.stringify(filteredAllUsers));
       setAppointments(
         JSON.parse(localStorage.getItem(`${service.title} appointments`))
       );
-      //sweet alert
+      Swal.fire({
+        title: "Success!",
+        text: "Your appointment has been booked!",
+        icon: "success",
+        confirmButtonText: "OK",
+      });
     }
   };
 
@@ -118,33 +116,6 @@ function ReservationForm({service}) {
       <form onSubmit={handleSubmit}>
         <div>
           <label>Type of Service: {service.title}</label>
-        </div>
-        <div>
-          <label>Full Name</label>
-          <input
-            type="text"
-            value={loggedUserNow.firstName + " " + loggedUserNow.lastName}
-            readOnly
-          />
-        </div>
-        <div>
-          <label>Email</label>
-          <input
-            type="text"
-            value={loggedUserNow.email}
-            readOnly
-          />
-        </div>
-        <div>
-          <label>Mobile Number</label>
-          <input
-            name="mobile"
-            type="tel"
-            value={reservation.mobile}
-            onChange={handleChange}
-            required
-            min="10"
-          />
         </div>
         <div>
           <label>Date of Service</label>
@@ -182,9 +153,22 @@ function ReservationForm({service}) {
           />
         </div>
         <div>
-          <label>Total Price:{reservation.startTime && reservation.finishTime ? (((Number(newFTime) - Number(newSTime)) / 100) *service.price) : 0}</label>
+          <label>
+            Total Price:
+            {reservation.startTime && reservation.finishTime
+              ? (
+                  ((Number(newFTime) - Number(newSTime)) / 100) *
+                  service.price
+                ).toFixed(2)
+              : 0}
+          </label>
         </div>
-    );
+        <button type="submit" className="bookBtn">
+          Book
+        </button>
+      </form>
+    </div>
+  );
 }
 
 export default ReservationForm;
